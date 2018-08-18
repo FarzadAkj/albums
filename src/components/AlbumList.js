620b390544ea6dbec47744e4812d7bb756afadee
FarzadAkj <farzad2116@gmail.com>
2018-08-18 11:55:46 +0430

    first CardItem is done, going to build second

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import Card from './Card';
+import CardItem from './CardItem';
+
+// https://teamairship.com/wp-content/uploads/2017/10/react-native-workshop.jpg
 
 renderAlbums = (album) => {
   return (
     <Card key={album.url}>
-      <Text>
-        {album.title}
-      </Text>
+      <CardItem>
+        <View style={styles.thubmnailContainerStyle}>
+          <Image
+            source={{ uri: "https://teamairship.com/wp-content/uploads/2017/10/react-native-workshop.jpg" }}
+            style={{ width: 50, height: 50, borderRadius: 5 }}
+            />
+        </View>
+
+        <View style={styles.headerContainerStyle}>
+          <Text style={{ fontSize: 18 }}>{album.title}</Text>
+          <Text>{album.artist}</Text>
+        </View>
+      </CardItem>
     </Card>
   );
 }
@@ -19,6 +32,20 @@ const AlbumList = (props) => {
       {props.data.map(album => renderAlbums(album))}
     </View>
   );
+}
+
+const styles = {
+  headerContainerStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    margin: 2,
+  },
+  thubmnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
 }
 
 export default AlbumList;
